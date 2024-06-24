cb5af1f184a62bafc98d5cec7eeaf478cc84b102
ghkdxofla <ghkdxofla@gmail.com>
2024-06-24 19:28:10 +0900

    feat: 03/11, 03/12

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,64 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+pub struct Order {
+    product_name: String,
+    quantity: u64,
+    unit_price: u64,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u64, unit_price: u64) -> Self {
+        Self {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn total(&self) -> u64 {
+        self.quantity * self.unit_price
+    }
+    fn validate_product_name(product_name: &str) -> bool {
+        !product_name.is_empty() && product_name.len() <= 300
+    }
+    pub fn set_product_name(&mut self, product_name: String) {
+        Order::validate_product_name(&product_name);
+        self.product_name = product_name;
+    }
+
+    pub fn product_name(&self) -> &str {
+        &self.product_name
+    }
+
+    fn validate_quantity(quantity: u64) -> bool {
+        quantity > 0
+    }
+
+    pub fn set_quantity(&mut self, quantity: u64) {
+        Order::validate_quantity(quantity);
+        self.quantity = quantity;
+    }
+
+    pub fn quantity(&self) -> &u64 {
+        &self.quantity
+    }
+
+    fn validate_unit_price(unit_price: u64) -> bool {
+        unit_price % 50 == 0 ||
+            unit_price % 25 == 0 ||
+            unit_price % 10 == 0 ||
+            unit_price % 5 == 0 ||
+            unit_price % 1 == 0 ||
+            unit_price > 0
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u64) {
+        Order::validate_unit_price(unit_price);
+        self.unit_price = unit_price;
+    }
+
+    pub fn unit_price(&self) -> &u64 {
+        &self.unit_price
+    }
+}
