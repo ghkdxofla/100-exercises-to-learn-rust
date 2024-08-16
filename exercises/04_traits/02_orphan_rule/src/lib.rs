847d8b6543e2ac51abbe974b8fabd310cbad6361
ghkdxofla <ghkdxofla@gmail.com>
2024-08-16 16:39:39 +0900

    feat: 04/02 ~ 05 trait

--- exercises/04_traits/02_orphan_rule/src/lib.rs
+++ exercises/04_traits/02_orphan_rule/src/lib.rs
@@ -4,8 +4,16 @@
 //  Look at the compiler error to get familiar with what it looks like.
 //  Then delete the code below and move on to the next exercise.
 
-impl PartialEq for u32 {
-    fn eq(&self, _other: &Self) -> bool {
-        todo!()
-    }
-}
+/**
+   Compiling orphan v0.1.0 (D:\Google Drive\project\100-exercises-to-learn-rust\exercises\04_traits\02_orphan_rule)
+error[E0117]: only traits defined in the current crate can be implemented for primitive types
+ --> exercises\04_traits\02_orphan_rule\src\lib.rs:7:1
+  |
+  | ^^^^^---------^^^^^---
+  | |    |             |
+  | |    `u32` is not defined in the current crate
+  |
+  = note: define and implement a trait or new type instead
+
+For more information about this error, try `rustc --explain E0117`.
+*/
